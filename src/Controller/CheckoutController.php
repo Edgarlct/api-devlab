8419241f729b2e029ecc172991b2c8b1f122f06d
Edouard <edouard.nicolas@edu.devinci.fr>
2022-04-29 19:40:23 +0200

    [add] treat command and create ticket

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -16,7 +16,7 @@ use Symfony\Config\Framework\RequestConfig;
 
 class CheckoutController extends AbstractController
 {
-    public function __construct(private EventRepository $eventRepository,private EntityManagerInterface $entityManager)
+    public function __construct(private EventRepository $eventRepository, private EntityManagerInterface $entityManager)
     {
     }
 
@@ -24,44 +24,47 @@ class CheckoutController extends AbstractController
         path: '/api/checkout',
         name: 'app_checkout',
         methods: ['POST'])]
-
-
-
-    public function index(Request $request, UserInterface $user, ): Response
+    public function index(Request $request, UserInterface $user): Response
     {
 
 
         $jsonBody = json_decode($request->getContent(), true);
-        $this->treatCommand($jsonBody, $user);
 
+        $treatReturn = $this->treatCommand($jsonBody, $user);
 
-        return $this->json('ok')->setStatusCode(200);
+        return $this->json($treatReturn)->setStatusCode(200);
     }
 
 
     //trete la commande
-    public function treatCommand($requestBody, $user){
-        if (isset($requestBody["eventId"])){
+    public function treatCommand($requestBody, $user)
+    {
+        if (isset($requestBody["eventId"])) {
             $event = $this->eventRepository->find($requestBody["eventId"]);
 
             //place gratuite
-            if($event->getPrice() == 0 && $event->getPlaceRemaining() > 0){
+            if ($event->getPrice() === 0 && $event->getPlaceRemaining() > 0) {
                 $this->createTicket($event, $user);
-                return $this->json('place creer')->setStatusCode(200);
+                return ["success" => true, "needBilling" => "false"];
             }
-            elseif($event->getPrice() > 0 && $event->getPlaceRemaining() > 0) {
-                //place paillante
+            //place paillante
+            elseif ($event->getPrice() > 0 && $event->getPlaceRemaining() > 0) {
+                $this->createTicket($event, $user);
+                return ["success" => false, "needBilling" => "true", "stripe" => $this->createCheckout()];
             }
-            elseif ($event->getPlaceRemaining() < 0){
-                    //plsu de place
+
+            elseif ($event->getPlaceRemaining() < 0) {
+                //plsu de place
             }
-            else{
+
+            else {
                 //erreur inconu
             }
         }
     }
-
-    public function createTicket($event, $user){
+    
+    public function createTicket($event, $user)
+    {
         $ticket = new Ticket();
         $ticket
             ->setUser($user)
@@ -70,6 +73,26 @@ class CheckoutController extends AbstractController
 
         $this->entityManager->persist($ticket);
         $this->entityManager->flush();
+    }
+
+    public function createCheckout()
+    {
+
+        \Stripe\Stripe::setApiKey('sk_test_51KskwVJ36F2VD1N9Eb9d6GtbhpTdqug6WNJL2caczsoHxucIfFHJm6WQPECvqfndFPZXfjUDwSerd4aWzpe85vTn00Oef6lHkm');
+
+        $paymentIntent = \Stripe\PaymentIntent::create([
+            'amount' => 1099,
+            'currency' => 'eur',
+            'payment_method_types' => ['bancontact', 'card', 'ideal', 'klarna', 'sepa_debit', 'sofort']]);
+
+          return [
+              'paymentIntent' => $paymentIntent->client_secret,
+              'paymentId' => $paymentIntent->id,
+              'publishableKey' => 'pk_test_51KskwVJ36F2VD1N9CFZJl9lajxZhG0OKoWTGbWhJa3D4fyWt5S6izP4khu9QjgALQruezdDJPcJvYegCJB1LLmyO00x2qUhOQe'
+          ];
+
+
+
     }
 
 }
